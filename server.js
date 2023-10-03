8fe842cfe25a8ce344e98e46bc1ef21de5c4a294
hwadong119 <jeonghwayun0119@naver.com>
2023-10-03 16:52:55 +0900

    Post 요청으로 데이터 추가하기

--- server.js
+++ server.js
@@ -2,26 +2,37 @@ const http = require('http');
 
 const port = 4000;
 
+const targetObject = {a: 'a', b: 'b'};
+
 const server = http.createServer((req, res) => {
-  if(req.url === '/home') {
-    res.writeHead(200, {
-      'Content-Type': 'application/json'
-    });
-    res.end(JSON.stringify({a: 'a', b: 'b'}));
-  } else if(req.url === '/about') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<body>');
-    res.write('<h1>About Page</h1>');
-    res.write('</body>');
-    res.write('</html>');
+  if (req.method === 'POST' && req.url === '/home') {
+    req.on('data', (data) => {
+      console.log(data);
+      const stringifiedData = data.toString();
+      console.log(stringifiedData);
+      Object.assign(targetObject, JSON.parse(stringifiedData));
+    })
   } else {
-    res.statusCode = 400;
-    res.end();
+    if(req.url === '/home') {
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+      res.end(JSON.stringify(targetObject));
+    } else if(req.url === '/about') {
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<html>');
+      res.write('<body>');
+      res.write('<h1>About Page</h1>');
+      res.write('</body>');
+      res.write('</html>');
+    } else {
+      res.statusCode = 400;
+      res.end();
+    }  
   }
-  
 })
 
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
+
