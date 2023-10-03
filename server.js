2f82209e1f1b395d53721b5bea781b4f7fd9a612
hwadong119 <jeonghwayun0119@naver.com>
2023-10-03 16:39:27 +0900

    웹 서버 생성

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const axios = require('axios');
+const http = require('http');
 
-axios.get('https://naver.com')
-  .then((response) => {console.log(response)})
-  .catch((error) => {console.log(error)})
+const port = 4000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  res.end(JSON.stringify({a: 'a', b: 'b'}));
+})
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+})
